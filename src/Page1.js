a6239cea4949c85648fd95d1bc5ce6e9aa6487be
mm2288 <47225267+mm2288@users.noreply.github.com>
2020-05-12 16:42:14 -0400

    Display accounts and transactions

--- src/Page1.js
+++ src/Page1.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
 import React from 'react';
+import css from './css/Page1.css';
+
+import AccountData from './data/AccountData.json';
 
 
 class Page1 extends React.Component {
 
-render() {
-  return(
-    <div>
-    <h1>This is the first page</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+      {AccountData.map((account, index) => {
+        return <div className="card">
+          <h1>{account.name}</h1>
+          <p>{account.balance}</p>
+        <button className="infoButton">More information</button>
+        </div>
+      })}
+
+      </div>
+    )
+  }
 }
 
-}
 
 
 
--- src/Page2.js
+++ src/Page2.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import axios from 'axios';
+import TransactionData from './data/TransactionData';
 
-const Page2 = props => {
-  return (
-    <div className="page-two">
-      <h2>Page 2: Can be shown through tabs</h2>
-    </div>
-  )
+class Page1 extends React.Component {
+
+  render() {
+    return (
+      <div>
+      {TransactionData.map((transactions, index) => {
+        return <div className="card">
+          <h1>{transactions.accountId}</h1>
+          <p>{transactions.type}</p>
+          <p>{transactions.amount}</p>
+          <p>{transactions.name}</p>
+        </div>
+      })}
+      </div>
+    )
+  }
 }
 
-export default Page2;
+
+
+
+export default Page1;
