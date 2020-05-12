5064d3fda32ec1a4774630c1f7ee7df0f54f3933
mm2288 <47225267+mm2288@users.noreply.github.com>
2020-05-12 15:08:23 -0400

    Created navigation using router

--- src/App.js
+++ src/App.js
@@ -1,64 +1,87 @@
-import React, { Component, Fragment } from 'react';
-import { Link, withRouter } from "react-router-dom";
-import { Nav, Navbar, NavItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
-import "./App.css";
+import React from 'react';
+import axios from 'axios';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
+import PageTabs from './PageTabs';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
+import VariablePage from "./VariablePage";
 
-        this.state = {
-            isAuthenticated: false
-        };
-
-        this.authenticate = this.authenticate.bind(this);
-        this.handleLogout = this.handleLogout.bind(this);
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <PageTabs/>
+        <div>
+          <Route path="/" exact component={Page1} />
+          <Route path="/page2" component={Page2} />
+          <Route path="/page3" component={Page3} />
+          <Route path="/page/:id" component={VariablePage} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-    authenticate(authenticated) {
-      this.setState({ isAuthenticated: authenticated });
+class AppOld extends React.Component {
+  state = {
+    view: 'page1',
+
+
+    /*new*/
+    accounts: [],
+    transactions:[],
+    newID: 0
+    /*new*/
+  }
+
+  /*new*/
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData(){
+  axios.get('https://my-json-server.typicode.com/ae324/is322_finalproject/db')
+      .then(response => {
+        this.setState({ allTasks: response.data, sortedTasks: this.sortTasks(response.data) });
+      }).catch (error => {
+    this.setState({ errorMessage: error.message });
+  });
+}
+  /*new*/
+
+
+  onViewChange(view) {
+    this.setState({ view });
+  }
+
+  wrapPage(jsx) {
+    const { view } = this.state;
+    return (
+      <div className="container">
+        <PageTabs currentView={view}
+                  onViewChange={this.onViewChange.bind(this)}/>
+        {jsx}
+      </div>
+    );
+  }
+
+  render() {
+    const { view } = this.state;
+
+    switch (view) {
+      case 'page1':
+        return (this.wrapPage(<Page1 />));
+      case 'page2':
+        return (this.wrapPage(<Page2 />));
+      case 'page3':
+        return (this.wrapPage(<Page3 />));
+      default:
+        return (this.wrapPage(<h2>Invalid Tab, choose another</h2>));
     }
 
-    handleLogout(){
-        this.authenticate(false);
-        this.props.history.push("/login");
-    }
-
-    render() {
-        const childProps = {
-          isAuthenticated: this.state.isAuthenticated,
-          authenticate: this.authenticate
-        };
-
-        return (
-            <div className="container">
-                <Navbar fluid collapseOnSelect>
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <Link to="/">HOME</Link>
-                        </Navbar.Brand>
-                    </Navbar.Header>
-                    <Navbar.Collapse>
-                        <Nav pullRight>
-                            {this.state.isAuthenticated
-                                ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
-                                : <Fragment>
-                                    <LinkContainer to="/login">
-                                        <NavItem>Login</NavItem>
-                                    </LinkContainer>
-                                    <LinkContainer to="/signup">
-                                        <NavItem>Signup</NavItem>
-                                    </LinkContainer>
-                                </Fragment>
-                            }
-                        </Nav>
-                     </Navbar.Collapse>
-                </Navbar>
-                <Routes childProps={childProps}/>
-            </div>
-        );
-    }
+  }
 }
 
-export default withRouter(App);
+export default App;
---
+++ src/Page1.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import React from 'react';
+
+
+class Page1 extends React.Component {
+
+render() {
+  return(
+    <div>
+    <h1>This is the first page</h1>
+    </div>
+  );
+}
+
+}
+
+
+
+export default Page1;
